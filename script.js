72bb5c8473d35d64356d01704bb1cdb3c1a1aa62
1netr0 <alexander8192@web.de>
2025-12-09 14:41:33 +0100

    die funktion displaySinglePokemon(pokemon) zeigt jetzt die pokemon in detailansicht an #3

--- script.js
+++ script.js
@@ -26,6 +26,34 @@ pokemonCards.id = "pokemon-cards";
 pokemonCards.classList.add("container");
 document.body.appendChild(pokemonCards);
 
+
+// Detail-View vor der Schleife erstellen
+let div = document.createElement("div");
+div.id = "pokemon-detail-view";
+div.classList.add("container");
+div.classList.add("d-none");
+document.body.appendChild(div);
+
+// Anzeige-Funktion außerhalb der Schleife
+function displaySinglePokemon(pokemon) {
+    const detailView = document.getElementById("pokemon-detail-view");
+    detailView.innerHTML = "";
+
+    const detailTitle = document.createElement("h2");
+    detailTitle.textContent = pokemon.name;
+
+    const detailImage = document.createElement("img");
+    detailImage.src = pokemon.image;
+
+    const detailType = document.createElement("p");
+    detailType.textContent = "Typ: " + pokemon.type;
+
+    detailView.appendChild(detailTitle);
+    detailView.appendChild(detailImage);
+    detailView.appendChild(detailType);
+}
+
+
 for (const pokemon of pokemons) {
     const card = document.createElement("div");
     card.classList.add("card");
@@ -39,39 +67,19 @@ for (const pokemon of pokemons) {
     const type = document.createElement("p");
     type.textContent = "Typ: " +  pokemon.type;
 
+    if (pokemon.type == "Feuer") type.style.color = "red";
+    if (pokemon.type == "Wasser") type.style.color = "blue";
+    if (pokemon.type == "Pflanze") type.style.color = "green";
+
     card.appendChild(title);
     card.appendChild(image);
     card.appendChild(type);
 
-    if (pokemon.type == "Feuer") {
-        type.style.color = "red";
-    }
-    if (pokemon.type == "Wasser") {
-        type.style.color = "blue";
-    }
-    if (pokemon.type == "Pflanze") {
-        type.style.color = "green";
-    }
-
-    function displaySinglePokemon(pokemon) {
-        const detailView = document.getElementById("pokemon-detail-view");
-        detailView.innerHTML = "";
-    }
-
-
-    card.addEventListener("click", function(){
-        console.log("Das Pokemon " + pokemon.name + " wurde geklickt");
-        pokemonCards.classList.toggle("d-none");
-        card.id = "pokemon-detail-view"
+    card.addEventListener("click", function () {
+        pokemonCards.classList.add("d-none");
+        div.classList.remove("d-none");
+        displaySinglePokemon(pokemon);
     });
 
     pokemonCards.appendChild(card);
 }
-
-let div = document.createElement("div");
-div.id = "pokemon-detail-view";
-div.classList.add("container");
-div.classList.add("d-none");
-document.body.appendChild(div);
-// Funktionen
-// Event Listener
